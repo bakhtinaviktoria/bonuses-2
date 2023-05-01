c03606f9288680518eaf60bb8329cc9f79ee092c
Viktoria B <bakhtina-vo@yandex.ru>
2023-05-01 18:08:21 +0300

    Main change

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int initialAmount = 100; // начальная сумма
-        int replenishmentAmount = 5000; // сумма пополнения
+        int replenishmentAmount = 4500; // сумма пополнения
         int balance = initialAmount + replenishmentAmount; // Итоговый баланс
 
         int bonus;
         if (replenishmentAmount >= 1000) {
             bonus = replenishmentAmount / 100;
-            System.out.println("Итоговая сумма " + (balance + bonus) + " рублей.");
+            System.out.println("Итоговая сумма " + balance + " рублей. Бонусных рублей из них " + bonus +".");
         } else
             System.out.println("Итоговая сумма " + balance + " рублей.");
     }
